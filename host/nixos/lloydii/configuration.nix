937e10f8b43a42f64146f10829f2b095ed272bc7
HyloArch <92276260+HyloArch@users.noreply.github.com>
2026-01-17 15:28:32 -0600

    Initial System Configs

---
+++ host/nixos/lloydii/configuration.nix
@@ -0,0 +1,19 @@
+{ config, lib, pkgs, ... }:
+{
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  system.stateVersion = "25.11";
+}
+
