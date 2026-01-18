fb10f0bed0857b754c76e51d5b2aa3eaef8a7168
HyloArch <92276260+HyloArch@users.noreply.github.com>
2026-01-17 18:29:41 -0600

    Random stuff

--- host/nixos/lloydii/configuration.nix
+++ host/nixos/lloydii/configuration.nix
@@ -3,12 +3,8 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  environment.systemPackages = with pkgs; [
-    nur.repos.xddxdd.pterodactyl-wings
-  ];
-
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [ 8493 ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
--- host/nixos/lloydii/default.nix
+++ host/nixos/lloydii/default.nix
@@ -3,6 +3,7 @@
   system = "x86_64-linux";
   modules = [
     (getModule /ssh.nix)
+    (getModule /wings.nix)
   ];
   users.hyloarch = {
     homeDirectory = "/home/hyloarch";
