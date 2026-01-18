b1179024e48c47173b5c6591e36baeb036473092
HyloArch <92276260+HyloArch@users.noreply.github.com>
2026-01-18 16:11:57 -0600

    Setup wings systemd service

--- mod/wings.nix
+++ mod/wings.nix
@@ -5,5 +5,24 @@
       nur.repos.xddxdd.pterodactyl-wings
     ];
     virtualisation.docker.enable = true;
+
+    systemd.services.wings = {
+      description = "Pterodactyl Wings Daemon";
+      after = [ "docker.service" ];
+      requires = [ "docker.service" ];
+      partOf = [ "docker.service" ];
+      serviceConfig = {
+        User = "root";
+        WorkingDirectory = "/etc/pterodactyl";
+        LimitNOFILE = 4096;
+        PIDFile = "/var/run/wings/daemon.pid";
+        ExecStart = "${pkgs.nur.repos.xddxdd.pterodactyl-wings}/bin/wings";
+        Restart = "on-failure";
+        StartLimitInterval = 180;
+        StartLimitBurst = 30;
+        RestartSec = 5;
+      };
+      wantedBy = [ "multi-user.target" ];
+    };
   };
 }
