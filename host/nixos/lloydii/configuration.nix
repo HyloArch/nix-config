81357bc2875f04784f57843eb363d8dca7fd7b00
HyloArch <92276260+HyloArch@users.noreply.github.com>
2026-01-17 19:02:12 -0600

    Open necessary port

--- host/nixos/lloydii/configuration.nix
+++ host/nixos/lloydii/configuration.nix
@@ -4,7 +4,10 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 8493 ];
+  networking.firewall.allowedTCPPorts = [
+    8493
+    8080
+  ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
