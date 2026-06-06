5c4a830bc7bd8ae98ed119da5290bb0a1044a69d
HyloArch <92276260+HyloArch@users.noreply.github.com>
2026-06-06 16:44:59 -0500

    Modify configuration

--- host/nixos/lloydii/configuration.nix
+++ host/nixos/lloydii/configuration.nix
@@ -4,10 +4,24 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [
-    8493
-    8080
-  ];
+  networking.firewall = {
+    allowedTCPPorts = [
+      8493
+      8080
+    ];
+  #  allowedUDPPortRanges = [
+  #    {
+  #      from = 28000; 
+  #      to = 28010;
+  #    }
+  #  ];
+  #  allowedTCPPortRanges = [
+  #    {
+  #      from = 28000;
+  #      to = 28010;
+  #    }
+  #  ];
+  };
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
